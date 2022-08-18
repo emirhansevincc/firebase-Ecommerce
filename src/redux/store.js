2197cd42eb2ce5b10710b49f1b7de4108033e954
Emirhan Sevinç <85807749+emirhansevincc@users.noreply.github.com>
2022-08-18 23:12:18 +0300

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { productsSlice } from "./productsSlice/productsSlice";
-
-export const store = configureStore({
-    reducer: {
-        product: productsSlice.reducer
-    }
-})
